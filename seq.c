1777ceea7d31733841d7c32df39e35ad360d7028
Jyoti Lall <jpl@jyoti.lall>
2021-10-04 09:19:43 -0400

    update some errors

--- seq.c
+++ seq.c
@@ -12,7 +12,7 @@
 #define READ_GROUP "239.0.0.1"
 #define WRITE_GROUP "239.0.0.2"
 
-main(int argc)
+int main(int argc)
 {
   struct sockaddr_in saddr;
   struct sockaddr_in raddr;
@@ -25,12 +25,12 @@ main(int argc)
   sockread = socket(AF_INET, SOCK_DGRAM, 0);
   if (sockread < 0) {
     perror("socket");
-    exit(1);
+    return(1);
   }
   sockwrite = socket(AF_INET, SOCK_DGRAM, 0);
   if (sockwrite < 0) {
     perror("socket");
-    exit(1);
+    return(1);
   }
 
   int reuse = 1;
@@ -66,7 +66,7 @@ main(int argc)
 	if (bind(sockread, (struct sockaddr *) &raddr, sizeof(raddr)) < 0) 
 	{        
 		perror("bind");
-	 	exit(1);
+	 	return(1);
   }    
 
   mreq.imr_multiaddr.s_addr = inet_addr(READ_GROUP);         
@@ -74,7 +74,7 @@ main(int argc)
   if (setsockopt(sockread, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) 
 	{
 		perror("setsockopt mreq");
-	 	exit(1);
+	 	return(1);
 	}         
 	
   while (1) 
@@ -84,7 +84,7 @@ main(int argc)
 	 if (cnt < 0) 
 	 {
 	    perror("recvfrom");
-	    exit(1);
+	    return(1);
 	 } else if (cnt == 0) 
    {
  	    break;
@@ -94,7 +94,7 @@ main(int argc)
 	 if (cnt < 0) 
 	 {
  	   perror("sendto");
-	   exit(1);
+	   return(1);
 	 }
 	 printf("r:%s w:%s \"%s\"\n", inet_ntoa(raddr.sin_addr), inet_ntoa(saddr.sin_addr), smessage);
   }
